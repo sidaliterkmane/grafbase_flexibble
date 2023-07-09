a874be4ab259cb0eabbaa459a2b77ee0b1361b40
Sid Ali Terkmane <sidterkmane@hotmail.com>
2023-07-09 09:27:24 -0400

    added google auth functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import AuthProviders from './AuthProviders'
+import { getCurrentUser } from '@/lib/session'
+import { signOut } from 'next-auth/react'
+import ProfileMenu from './ProfileMenu'
 
-const Navbar = () => {
-    const session = {}
+const Navbar = async () => {
+    const session = await getCurrentUser();
 
     return (
         <nav className='flexBetween navbar'>
@@ -29,14 +32,14 @@ const Navbar = () => {
             </div>
 
             <div className='flexCenter gap-4'>
-                {session ? (
+                {session?.user ? (
                     <>
-                        UserPhoto
+                        <ProfileMenu session={session}/>
 
                         <Link href='/create-project'>
                             Share Work
                         </Link>
-                    </>
+                    </>    
                 ) : (
                     <AuthProviders />
                 )}
